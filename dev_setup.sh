68e6250a75e8430e45376e081350ecb3e708adc6
James Woglom <j@wogloms.net>
2020-09-17 23:23:24 -0400

    fix expired key while building, run collectstatic in setup, bypass postgres error

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,6 +3,8 @@
 docker-compose build
 # Do this in the mounted directory, since the Dockerfile did it in a folder we
 # mask by mounting over it.
-docker-compose run web sh -c "rm -rf bower_components && cd /tmp && python /code/manage.py bower install && mv bower_components /code/"
+docker-compose run web sh -c "rm -rf bower_components && cd /tmp && python /code/manage.py bower install && python /code/manage.py collectstatic --noinput && mv bower_components /code/"
 # Stop everything again.
 docker-compose stop
+
+echo "To start cloudpebble, run: docker-compose up"
